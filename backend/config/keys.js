53207c3dcaa4c141976cdb90ea0f906e67bdd08b
fathimaththasneem <fathimaththasnee@gmail.com>
2024-07-05 12:07:00 +0530

    full code

---
+++ backend/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/mydatabase', 
+    googleClientID: '1091800048753-rlmdt536f1duv890fhon8c0iut67ae9c.apps.googleusercontent.com', 
+    googleClientSecret: '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', 
+    cookieKey: 'your_cookie_key' 
+  };
+  
---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  displayName: String,
+  email: String,
+});
+
+mongoose.model('users', userSchema);
+
